90d4d50d8bda5660796a9852787c8d8c0710bd6f
Shubhamdataman <shubhamdataman66@gmail.com>
2025-09-02 12:49:05 +0530

    fix: updated ResetPassword

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
     );
     console.log("DETAILS", updatedDetails);
 
-    const url = `https://snfrontend-7lyz.onrender.com//update-password/${token}`;
+    const url = `https://snfrontend-7lyz.onrender.com/update-password/${token}`;
 
     await mailSender(
       email,
